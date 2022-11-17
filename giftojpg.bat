ffc609f17478cae42ca3f8163c9e43b966b4b093
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2022-11-17 12:22:57 +0300

    Add files via upload

---
+++ giftojpg.bat
@@ -0,0 +1,10 @@
+@echo off
+color 2
+
+echo "Gif Dosyaları Jpg Dosyalarına Değiştiriliyor"
+
+ren *.gif *.jpg
+
+echo "Bütün resim dosyaları değiştirildi."
+echo
+pause
---
+++ giftopng.bat
@@ -0,0 +1,10 @@
+@echo off
+color 2
+
+echo "Gif Dosyaları Png Dosyalarına Değiştiriliyor"
+
+ren *.gif *.png
+
+echo "Bütün resim dosyaları değiştirildi."
+echo
+pause
